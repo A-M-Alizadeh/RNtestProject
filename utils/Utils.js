81e846aeb1b62504d124b5e4ff3929da377020e0
Ali <am_alizadeh74@yahoo.com>
2023-01-04 00:39:33 +0100

    moving to typescript

--- utils/Utils.js
+++ utils/Utils.js
@@ -2,37 +2,37 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const saveAsynce = async (key, value) => {
     try {
-        await AsyncStorage.setItem(key, value)
-      } catch (e) {
-        console.log('Error while saving in storage')
-      }
-}
+        await AsyncStorage.setItem(key, value);
+    } catch (e) {
+        console.log('Error while saving in storage');
+    }
+};
 const readAsync = async (key) => {
     try {
-        const value = await AsyncStorage.getItem(key)
+        const value = await AsyncStorage.getItem(key);
         console.log('readAsync', key, value);
         return value !== null ? value : null;
-      } catch(e) {
-        console.log('Error while reading from storage')
-      }
-}
+    } catch(e) {
+        console.log('Error while reading from storage');
+    }
+};
 
 const saveObjAsync = async (key, value) => {
-        try {
-          const jsonValue = JSON.stringify(value)
-          await AsyncStorage.setItem(key, jsonValue)
-        } catch (e) {
-            console.log('Error while saving in object storage')
-        }
-}
+    try {
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+        console.log('Error while saving in object storage');
+    }
+};
 const readObjAsync = async (key) => {
     try {
-        const jsonValue = await AsyncStorage.getItem(key)
+        const jsonValue = await AsyncStorage.getItem(key);
         return jsonValue != null ? JSON.parse(jsonValue) : null;
-      } catch(e) {
-        console.log('Error while reading object from storage')
-      }
-}
+    } catch(e) {
+        console.log('Error while reading object from storage');
+    }
+};
 
 
-export {saveAsynce, saveObjAsync, readAsync, readObjAsync}
+export {saveAsynce, saveObjAsync, readAsync, readObjAsync};
